b3f596d0f5ed8719c03698bff4b085ca7548c7fc
ndobrosavljevic <n.dobrosavljevic01@gmail.com>
2024-05-12 14:00:40 +0200

    refactor: rename main to master

--- src/main.cpp
+++ src/main.cpp
@@ -162,6 +162,7 @@ int main() {
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void) io;
+    
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330 core");

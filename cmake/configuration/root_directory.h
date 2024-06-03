ede91b44b711cbbb271333790a6f21f6f9af23b0
ndobrosavljevic <n.dobrosavljevic01@gmail.com>
2024-06-03 23:57:47 +0200

    refactor: CMakeLists

--- cmake/configuration/root_directory.h
+++
@@ -1,1 +0,0 @@
-const char * logl_root = "/home/ndobrosavljevic/Desktop/AZRS_zavrsna_verzija_projekta/computer_graphics";
--- format.sh
+++
@@ -1,1 +0,0 @@
-find . -name '*.h' -o -name '*.cpp' | xargs clang-format -i
